851871a8f79cee81d7a4a5534199e80cc2289658
Evan Keller <evankeller@pop-os.localdomain>
2023-11-23 17:36:41 -0700

    added JQuery and basic event listener to take song and artist name when submit button pressed

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+$('#submitButton').on('click', function () {
+    var songInput = $('#songInput').val().trim();
+    var artistInput = $('#artistInput').val().trim();
+    console.log(songInput);
+    console.log(artistInput);
+});
