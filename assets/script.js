79dc4b2992980cfe6154ef344418ad1fdd3e6940
Jadah Pierce <145732309+heyitsmejadah@users.noreply.github.com>
2023-11-27 20:46:07 -0700

    Added a start to the display recs function to display a list.

--- assets/script.js
+++ assets/script.js
@@ -46,10 +46,23 @@ $('#submitButton').on('click', function () {
 // Expand .click event to bring up either new HTML or append the HTML to display an aside of reccomendations
 // Feed API userdata to make a list of reccommended songs and or artists
 // Depending on the information we can pull, create section for stats like "dancability" on searched terms
+//Add function to create list items based off of what we pull from the api for recommended music
+function displayRecommendations(data) {
+    var recommendationsContainer = document.getElementById("lists-container");
+    //Clear previous recs
+    recommendationsContainer.innerHTML = "";
 
+    data.tracks.forEach(function (track){
+        //create a div for each rec
+        var recommendationItem = document.createElement("div");
+        recommendationItem.className = "recommendation-item";
+        recommendationItem.innerHTML = "<p>" + track.name + " by " + track.artists[0].name + "<p>";
+        recommendationsContainer.appendChild(recommendationItem);
+    });
+}
 // Add home button to go back to search page
 document.getElementById("homeButton").addEventListener("click", function (){
-    window.location.href = "ADD SEARCH PAGE HTML";
+    window.location.href = "index.html";
 });
 // Add an information API, like WIKI or another Song based, to display lyrics or information depending on the selected song or artist
 // Add a backup for when artist or song has no additional information 
